773abc352219f0dd8966285e8db13e27f42a0bed
j1348 <me@jfroffice.me>
2017-01-11 20:25:29 +0100

    Refactor database code

--- src/database.js
+++ src/database.js
@@ -1,7 +1,6 @@
 const Promise = require('bluebird');
 const mongoose = require('mongoose');
 const path = require('path');
-const DB_URI = getDatabaseURI();
 
 const db = {
   User: Promise.promisifyAll(require(path.join(__dirname, 'entities/user/user-model'))),
@@ -9,43 +8,18 @@ const db = {
 };
 
 // connect to the database
-mongoose.connect(DB_URI);
+mongoose.connect(process.env.MONGODB);
 
-/**
- *
- * When the database is ready, log it!
- *
- */
 function onDatabaseConnection() {
   console.log('Database connection is open!');
 }
 
-/**
- *
- * When the database is disconnected, log it!
- *
- */
 function onDatabaseDisconnection() {
   console.log('Database connection is lost');
 }
 
-/**
- *
- * When the database has an error, log it!
- *
- */
 function onDatabaseError(err) {
   console.log(`Database connection has an error: ${err}`);
-}
-
-/**
- *
- * Get the DB_URI based on NODE_ENV
- *
- */
-function getDatabaseURI() {
-  return process.env.MONGODB;
-    // return 'mongodb://' + (process.env.MONGO_1_PORT_27017_TCP_ADDR || '127.0.0.1') + '/dbHapiTest'; //process.env.NODE_ENV === 'test' ? 'mongodb://localhost/dbHapiTest' : process.env.DB_URI;
 }
 
 db.database = mongoose.connection;
