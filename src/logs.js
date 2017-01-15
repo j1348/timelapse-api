9e9004abfeebad2072e41776fb6d86513b375e14
j1348 <me@jfroffice.me>
2017-01-15 17:39:22 +0100

    Add good-sentry reporter only if process.env.SENTRY_DSN is defined

--- src/logs.js
+++ src/logs.js
@@ -1,46 +1,47 @@
 const Good = require('good');
 const path = require('path');
 const release = require(path.join(__dirname, '../package')).version;
+const reporters = {
+    myConsoleReporter: [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ error: '*', log: '*', response: '*', request: '*' }]
+        },
+        {
+            module: 'good-console'
+        },
+        'stdout'
+    ]
+};
+
+if (process.env.SENTRY_DSN) {
+    reporters.mySentryReporter = [{
+            module: 'good-squeeze',
+            name: 'Squeeze',
+            args: [{ error: '*', log: '*', response: '*', request: '*' }],
+        }, {
+            module: 'good-sentry',
+            args: [{
+                dsn: process.env.SENTRY_DSN,
+                config: {
+                    name: 'timelapse-api',
+                    logger: 'mySentryReporter',
+                    release,
+                    environment: process.env.NODE_ENV
+                }
+            }]
+        }];
+}
 
 exports.register = (server, options, next) => {
-    const opts = {
-        ops: {
-            interval: 1000
-        },
-        reporters: {
-            myConsoleReporter: [{
-                    module: 'good-squeeze',
-                    name: 'Squeeze',
-                    args: [{ error: '*', log: '*', response: '*', request: '*' }]
-                },
-                {
-                    module: 'good-console'
-                },
-                'stdout'
-            ],
-            mySentryReporter: [{
-                    module: 'good-squeeze',
-                    name: 'Squeeze',
-                    args: [{ error: '*', log: '*', response: '*', request: '*' }],
-                }, {
-                    module: 'good-sentry',
-                    args: [{
-                        dsn: process.env.SENTRY_DSN,
-                        config: {
-                            name: 'timelapse-api',
-                            logger: 'mySentryReporter',
-                            release,
-                            environment: process.env.NODE_ENV
-                        }
-                    }]
-                }
-            ]
-        }
-    };
-
     server.register({
         register: Good,
-        options: opts,
+        options: {
+            reporters,
+            ops: {
+                interval: 1000
+            }
+        }
     }, err => next(err));
 };
 
