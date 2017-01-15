edf82ea10ba7b41bbd1de456ef242d7df3ef4296
j1348 <me@jfroffice.me>
2017-01-15 18:53:00 +0100

    Restore User API test

--- src/entities/user/user-routes.js
+++ src/entities/user/user-routes.js
@@ -6,19 +6,7 @@ exports.register = (server, options, next) => {
   const controller = new Controller(options.database);
 
   server.bind(controller);
-  server.route([
-    // {
-    //   method: 'GET',
-    //   path: '/user',
-    //   config: {
-    //     tags: ['api'], // ADD THIS TAG
-    //     description: 'get user list',
-    //     notes: 'get user list',
-    //     auth: false,
-    //     handler: controller.list,
-    //     validate: Validator.list(),
-    //   },
-    // },
+  const routes = [
     {
       method: 'GET',
       path: '/user/{id}',
@@ -64,7 +52,24 @@ exports.register = (server, options, next) => {
         validate: Validator.destroy(),
       },
     },
-  ]);
+  ];
+
+  if (process.env.NODE_ENV === 'test') {
+    routes.push({
+      method: 'GET',
+      path: '/user',
+      config: {
+        tags: ['api'], // ADD THIS TAG
+        description: 'get user list',
+        notes: 'get user list',
+        auth: false,
+        handler: controller.list,
+        validate: Validator.list(),
+      },
+    });
+  }
+
+  server.route(routes);
 
   next();
 };
